df5f43bbb1ebaa0eb6ff2b160478af49c01c27bd
Masato Yamashita <m.yamashii@gmail.com>
2020-04-01 19:33:49 +0900

    Delete SystemEnvironmentSettings. And modify inventory file path to  execute ansible playbook.

--- SetupStart.sh
+++ SetupStart.sh
@@ -8,13 +8,10 @@ echo "Install Ansible." 2>&1 | tee -a InstallLog.log
 zsh ./setup/AnsibleInstall.sh | tee -a InstallLog.log
 
 echo "Execute ansible playbook." 2>&1 | tee -a InstallLog.log
-ansible-playbook ./setup/setup.yml -i inventory | tee -a InstallLog.log
+ansible-playbook ./setup/setup.yml -i ./setup/inventory | tee -a InstallLog.log
 
 echo "Shell Settings." 2>&1 | tee -a InstallLog.log
 zsh ./setup/ShellSettings.sh 2>&1 | tee -a InstallLog.log
-
-echo "System Environments Settings." 2>&1 | tee -a InstallLog.log
-zsh ./setup/SystemEnvironmentsSettings.sh | tee -a InstallLog.log
 
 echo "Setup finished." 2>&1 | tee -a InstallLog.log
 
